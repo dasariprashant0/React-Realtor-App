0a553d85ae4c4ccf979343c22feb493c9eaa3ae3
Prashant Dasari <dasariprashant0@gmail.com>
2023-07-04 17:02:26 +0530

    Created ReactJS and Installed TailwindCSS and setupped the tailwind config file.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-2xl bg-red-500 text-blue-800'>Hello</h1>
+      
+    </>
+  )
+}
+
+export default App

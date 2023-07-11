097d102fa1b0002f15b649496e7345345304b25e
Prashant Dasari <dasariprashant0@gmail.com>
2023-07-12 01:35:24 +0530

    Added the GoogleAuthProvider for Authentication another cool thing added

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,13 +1,47 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { auth, db } from "../Firebase";
+import { toast } from "react-toastify";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router";
 
 const OAuth = () => {
-  return (
-    <button className="flex justify-center items-center bg-red-700 w-full text-white rounded py-2 px-7 uppercase text-sm font-medium hover:bg-red-600 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out">
 
+  const navigate = useNavigate()
+
+  const onGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+  
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      toast.success("You have successfully signed in!");
+      navigate("/")
+    } catch (error) {
+      toast.error("Could not authorize with Google Authentication");
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={onGoogleClick}
+      className="flex justify-center items-center bg-red-700 w-full text-white rounded py-2 px-7 uppercase text-sm font-medium hover:bg-red-600 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out"
+    >
       <FcGoogle className="mr-2 bg-white rounded-full text-2xl" />
       Continue with Google
-      
     </button>
   );
 };

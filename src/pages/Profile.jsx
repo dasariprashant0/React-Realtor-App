3722469163fdff33e125601bb70464c335a90659
Prashant Dasari <dasariprashant0@gmail.com>
2023-07-13 02:29:33 +0530

    Added Edit Functionality to the Profile Page and Fixed a mistake in the Firestore rules which was giving error when performing edit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,24 +1,54 @@
 import React, { useState } from "react";
-import { auth } from "../Firebase";
-import { signOut } from "firebase/auth";
+import { auth, db } from "../Firebase";
+import { signOut, updateProfile } from "firebase/auth";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router";
+import { doc, updateDoc } from "firebase/firestore";
 
 const Profile = () => {
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
   const { name, email } = formData;
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const onSignOut = () => {
-    signOut(auth).then(() => {
-      toast.success("Signed Out Successfully.")
-      navigate("/sign-in")
-    }).catch((error) => {
-      toast.error("There was an error in Signing Out.")
-    })
+    signOut(auth)
+      .then(() => {
+        toast.success("Signed Out Successfully.");
+        navigate("/sign-in");
+      })
+      .catch((error) => {
+        toast.error("There was an error in Signing Out.");
+      });
+  };
+
+  function onChange(e) {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  async function onSubmit(e) {
+    try {
+      if (auth.currentUser.displayName != name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+      }
+
+      const docRef = doc(db, "users", auth.currentUser.uid);
+      await updateDoc(docRef, {
+        name,
+      });
+
+      toast.success("Changes Applied Successfully.");
+    } catch (error) {
+      toast.error("Could not apply changes. Please Try Again Later !!!")
+    }
   }
 
   return (
@@ -33,8 +63,11 @@ const Profile = () => {
             type="text"
             id="name"
             value={name}
-            disabled
-            className="text-input mb-6"
+            disabled={!changeDetail}
+            onChange={onChange}
+            className={`text-input mb-6 ${
+              changeDetail && "bg-red-200 focus:bg-red-200"
+            }`}
           />
 
           {/* Email Input */}
@@ -44,17 +77,28 @@ const Profile = () => {
             id="email"
             value={email}
             disabled
-            className="text-input mb-6"
+            className={`text-input mb-6`}
           />
 
           <div className="flex justify-between items-center whitespace-nowrap text-sm sm:text-lg mb-6">
             <p>
               Do you want to change your name?{" "}
-              <span className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1 cursor-pointer">
-                Edit
+              <span
+                onClick={() => {
+                  changeDetail && onSubmit();
+                  setChangeDetail((prevState) => !prevState);
+                }}
+                className="text-red-600 hover:text-red-700 transition duration-200 ease-in-out ml-1 cursor-pointer"
+              >
+                {changeDetail ? "Apply Change" : "Edit"}
               </span>
             </p>
-            <p className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out ml-1 cursor-pointer" onClick={onSignOut}>Sign out</p>
+            <p
+              className="text-blue-600 hover:text-blue-700 transition duration-200 ease-in-out ml-1 cursor-pointer"
+              onClick={onSignOut}
+            >
+              Sign out
+            </p>
           </div>
 
           <button className="flex justify-center items-center bg-blue-700 w-full text-white rounded py-2 px-7 uppercase text-sm font-medium hover:bg-blue-600 active:bg-blue-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out">
